abbaa95af19f79bbefb23b619940716ad5aa61a6
Manuel Fernandez <manferlo81@gmail.com>
2019-05-21 01:12:46 +0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # map-number
+
 processing/p5.js map like function
+
+## License
+
+[MIT License](LICENSE)
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+function map(num: number, min: number, max: number, outMin: number, outMax: number): number {
+  return (num - min) / (max - min) * (outMax - outMin) + outMax;
+}
+
+function floor(num: number, min: number, max: number, outMin: number, outMax: number) {
+  return Math.floor(
+    map(num, min, max, outMin, outMax)
+  );
+}
+
+function round(num: number, min: number, max: number, outMin: number, outMax: number) {
+  return Math.round(
+    map(num, min, max, outMin, outMax)
+  );
+}
+
+function limit(num: number, min: number, max: number, outMin: number, outMax: number) {
+  return Math.max(
+    outMin,
+    Math.min(
+      outMax,
+      map(num, min, max, outMin, outMax)
+    )
+  );
+}
+
+export { map, floor, round, limit }
