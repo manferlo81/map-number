0c5b50a6608d759ae0eb273372a341baf336231b
Manuel Fernandez <manferlo81@gmail.com>
2019-05-21 01:45:09 +0700

    jest config

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+const CI = process.env.CI;
+
+module.exports = {
+
+  testEnvironment: "node",
+
+  collectCoverage: false,
+  collectCoverageFrom: [
+    "dist"
+  ],
+  coverageDirectory: "coverage",
+  coverageReporters: [
+    CI ? "json" : "lcov",
+    "text",
+    "text-summary"
+  ],
+  coverageThreshold: null,
+
+  verbose: true,
+
+};
