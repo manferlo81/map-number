b950e4651f8afdd589b39cb19ef22c087a3c74c5
Manuel Fernandez <manferlo81@gmail.com>
2019-05-29 00:59:14 +0700

    added ceil method test

---
+++ test/ceil.test.js
@@ -0,0 +1,51 @@
+const { ceil } = require("..");
+
+describe("ceil method", () => {
+
+  test("should return in-range output", () => {
+
+    expect(
+      ceil(23, 20, 50, 2, 5)
+    ).toBe(
+      3
+    );
+
+  });
+
+  test("should return out-range output", () => {
+
+    expect(
+      ceil(6, 20, 50, 2, 5)
+    ).toBe(
+      1
+    );
+
+    expect(
+      ceil(62, 20, 50, 2, 5)
+    ).toBe(
+      7
+    );
+
+  });
+
+  test("should invert in-range output", () => {
+
+    expect(
+      ceil(3.1, 1, 9, 9, 1)
+    ).toBe(
+      7
+    );
+
+  });
+
+  test("should invert out-range output", () => {
+
+    expect(
+      ceil(-1.3, 1, 9, 9, 1)
+    ).toBe(
+      12
+    );
+
+  });
+
+});
