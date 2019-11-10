4e945ba4b332e11cc20fc624bbd37f575693995b
Manuel Fernandez <manferlo81@gmail.com>
2019-11-10 21:43:18 +0700

    code style & linting

--- jest.config.js
+++ jest.config.js
@@ -1,33 +1,33 @@
-const threshold = 95;
+const threshold = 95
 
 module.exports = {
 
-  testEnvironment: "node",
+  testEnvironment: 'node',
   browser: false,
 
-  cacheDirectory: "node_modules/.cache/jest",
+  cacheDirectory: 'node_modules/.cache/jest',
 
-  preset: "ts-jest",
+  preset: 'ts-jest',
 
   collectCoverage: true,
   collectCoverageFrom: [
-    "src/**/*.ts"
+    'src/**/*.ts',
   ],
-  coverageDirectory: "coverage",
+  coverageDirectory: 'coverage',
   coverageReporters: [
-    process.env.CI ? "json" : "lcov",
-    "text",
-    "text-summary"
+    process.env.CI ? 'json' : 'lcov',
+    'text',
+    'text-summary',
   ],
   coverageThreshold: {
     global: {
       branches: threshold,
       functions: threshold,
       lines: threshold,
-      statements: threshold
-    }
+      statements: threshold,
+    },
   },
 
   verbose: true,
 
-};
+}
