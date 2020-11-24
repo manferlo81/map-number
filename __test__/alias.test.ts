828bb051afce071b5f5bbbc83a36a91da500d285
Manuel Fernández <manferlo81@gmail.com>
2020-11-24 15:25:42 +0700

    chore(test): Add test for "transformed" alias

--- __test__/alias.test.ts
+++ __test__/alias.test.ts
@@ -1,14 +1,18 @@
-import { compile, create, wrap, limit, clamp } from '../src';
+import { compile, create, wrap, limit, clamp, transform, transformed } from '../src';
 
 describe('alias', () => {
 
-  test('Should export compile alias', () => {
+  test('Should export "compile" alias', () => {
     expect(create).toBe(compile);
     expect(wrap).toBe(compile);
   });
 
-  test('Should export limit alias', () => {
+  test('Should export "limit" alias', () => {
     expect(clamp).toBe(limit);
+  });
+
+  test('Should export "transformed" alias', () => {
+    expect(transform).toBe(transformed);
   });
 
 });
