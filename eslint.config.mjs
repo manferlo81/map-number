a7a0879beeed94818a115d4936c34b2e7a407e12
Manuel Fernández <manferlo81@gmail.com>
2024-09-08 18:13:11 +0700

    chore(lint): Refactor ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,62 +11,60 @@ function normalizeRuleEntry(entry) {
 
 function createRuleNameNormalizer(pluginName) {
   if (!pluginName) return (ruleName) => ruleName;
+  const pluginPrefix = `${pluginName}/`;
   return (ruleName) => {
-    const pluginPrefix = `${pluginName}/`;
     if (ruleName.startsWith(pluginPrefix)) return ruleName;
     return `${pluginPrefix}${ruleName}`;
   };
 }
 
-function rules(pluginName, rules) {
+function normalizeRules(pluginName, rules) {
   const normalizeRuleName = createRuleNameNormalizer(pluginName);
   return Object.fromEntries(
-    Object.entries(rules).map(([ruleName, value]) => {
-      return [normalizeRuleName(ruleName), normalizeRuleEntry(value)];
-    }),
+    Object.entries(rules).map(
+      ([ruleName, value]) => [normalizeRuleName(ruleName), normalizeRuleEntry(value)],
+    ),
   );
 }
 
-const eslintRules = rules(null, {
+const eslintRules = normalizeRules(null, {
   'no-useless-rename': 'error',
   'object-shorthand': 'error',
   'prefer-template': 'error',
 });
 
-const stylisticRules = rules('@stylistic', {
-  semi: 'always',
+const stylisticRules = normalizeRules('@stylistic', {
   indent: 2,
-  quotes: 'single',
   'linebreak-style': 'unix',
-
-  'quote-props': 'as-needed',
-  'arrow-parens': 'always',
   'no-extra-parens': 'all',
   'no-extra-semi': 'error',
-
-  'member-delimiter-style': {},
   'padded-blocks': 'off',
 });
 
-const typescriptRules = rules('@typescript-eslint', {
+const typescriptRules = normalizeRules('@typescript-eslint', {
   'restrict-template-expressions': 'off',
 });
 
-const javascriptExtensions = ['js', 'cjs', 'mjs'].join(',');
+const stylisticConfig = stylistic.configs.customize({
+  semi: true,
+  quotes: 'single',
+  quoteProps: 'as-needed',
+  arrowParens: true,
+  braceStyle: '1tbs',
+});
 
 const typescriptConfig = config(
   ...typescriptConfigs.strictTypeChecked,
   ...typescriptConfigs.stylisticTypeChecked,
   { languageOptions: { parserOptions: { projectService: true, tsconfigRootDir: process.cwd() } } },
-  { files: [`*.{${javascriptExtensions}}`], ...typescriptConfigs.disableTypeChecked },
+  { files: [`*.{js,cjs,mjs}`], ...typescriptConfigs.disableTypeChecked },
 );
 
 export default config(
   { ignores: ['dist', 'coverage'] },
-  { files: [`*.{${javascriptExtensions},ts}`] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   js.configs.recommended,
-  stylistic.configs['recommended-flat'],
+  stylisticConfig,
   ...typescriptConfig,
   { rules: { ...eslintRules, ...stylisticRules, ...typescriptRules } },
 );
