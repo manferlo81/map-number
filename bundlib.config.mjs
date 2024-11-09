19fe531f3e3e0c2b247bf9c67980d620bde928ee
Manuel Fernández <manferlo81@gmail.com>
2024-11-09 12:27:57 +0700

    chore: Use  `esModule` and `interop` options for cjs build

--- bundlib.config.mjs
+++ bundlib.config.mjs
@@ -1,6 +1,8 @@
 import { config } from 'bundlib';
 
 export default config({
+  esModule: true,
+  interop: true,
   name: 'mapNum',
   min: ['browser', 'module'],
   project: 'tsconfig-build.json',
